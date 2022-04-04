cf9766596e3e1acd9e8000d57c9cd8a4270606d7
Corey Bradshaw <cjabradshaw@gmail.com>
2022-04-04 12:28:27 +0930

    Update AnemometerFunction.R
    added pressure to plot

--- AnemometerFunction.R
+++ AnemometerFunction.R
@@ -13,15 +13,16 @@ cityfile <- jsonlite::fromJSON(gzcon(url("https://bulk.openweathermap.org/sample
 AnemometerFunc <- function(Place, PlaceFile=cityfile) # where 'Place' is a city character string ('city,2-digit ISO country code') e.g., 'Adelaide,AU'
 
 {
-  raw.dat <- capture.output(temp1 <- processx::run('ansiweather', c("-l", Place, "-w", "true", "-d", "false", "-p", "false", "-h",
+  raw.dat <- capture.output(temp1 <- processx::run('ansiweather', c("-l", Place, "-w", "true", "-d", "false", "-p", "true", "-h",
                                                                  "true", "-s", "false", "-a", "false"), error_on_status = FALSE, echo=T))
   dat <- scan(text = raw.dat, what = "")
-  hum <- dat[length(dat)] # % humidity
+  pres <- as.numeric(dat[length(dat)-1])
+  hum <- dat[length(dat)-4] # % humidity
   humN <- as.numeric(substr(hum,1,nchar(hum)-1))
-  wdir <- dat[length(dat)-3] # wind direction
-  wspeed <- as.numeric(dat[length(dat)-5]) # wind speed m/s
+  wdir <- dat[length(dat)-7] # wind direction
+  wspeed <- as.numeric(dat[length(dat)-9]) # wind speed m/s
   wspeed.kmhr <- round(wspeed*3600/1000, 1) # wind speed km/hr
-  temp <- as.numeric(dat[length(dat)-12])
+  temp <- as.numeric(dat[length(dat)-16])
   
   ## local time at Place
   Place2 <- scan(text=Place, what="", sep=",")
@@ -46,7 +47,8 @@ AnemometerFunc <- function(Place, PlaceFile=cityfile) # where 'Place' is a city 
   
   arrow.coords.dat <- data.frame(x0s, x1s, y0s, y1s, codex$dircode)
   dir.sub <- which(arrow.coords.dat$codex.dircode == wdir)
-
+  arrow.width <- wspeed.kmhr/10 # arrow line width increases with increasing wind speed
+  
   # temperature-dependent text colour
   tcolrs <- rev(RColorBrewer::brewer.pal(n=11, "RdBu"))[-c(5,6)]
   minT <- -15 # min temp for colour scale
@@ -68,14 +70,21 @@ AnemometerFunc <- function(Place, PlaceFile=cityfile) # where 'Place' is a city 
   hcols.dat <- data.frame(loHs, upHs, hcolrs)
   h.col <- hcols.dat[which(hcols.dat$loHs < humN & hcols.dat$upHs >= humN), ]$hcolrs # choose humidity colour (yellow to blue)
   
-  # plot anemometer
-  arrow.width <- wspeed.kmhr/10 # arrow line width increases with increasing wind speed
+  # pressure
+  prestxt1 <- ifelse(pres < 980, "very low", ifelse(pres > 980 & pres <= 1000, "low", "moderate"))
+  prestxt <- ifelse(pres > 1025, "high", prestxt1)
+  prescolr1 <- ifelse(prestxt == "very low", "dark red", ifelse(prestxt == "low", "red", "pink"))
+  prescolr <- ifelse(prestxt == "high", "orange", prescolr1)
+  
+  ## coordinates
   hemSN <- ifelse(sign(coords$lat)[1]==-1, "S", "N")
   hemEW <- ifelse(sign(coords$lon)[1]==-1, "W", "E")
   latDMS <- ifelse(hemSN=="S", paste(-ceiling(coords$lat), "º ", round((-(coords$lat-ceiling(coords$lat)))*60, 2), "′ ", hemSN, sep=""),
                    paste(floor(coords$lat), "º ", round(((coords$lat-floor(coords$lat)))*60, 2), "′ ", hemSN, sep=""))
   lonDMS <- ifelse(hemEW=="W", paste(-ceiling(coords$lon), "º ", round((-(coords$lon-ceiling(coords$lon)))*60, 2), "′ ", hemEW, sep=""),
                    paste(floor(coords$lon), "º ", round(((coords$lon-floor(coords$lon)))*60, 2), "′ ", hemEW, sep=""))
+  
+  # plot anemometer
   par(xaxt="n", yaxt="n", pty="s")
   plot(0:10,0:10,pch=NULL,col=NULL,xlab="",ylab="")
   arrows(x0=arrow.coords.dat[dir.sub,1], y0=arrow.coords.dat[dir.sub,3], x1=arrow.coords.dat[dir.sub,2], 
@@ -84,7 +93,8 @@ AnemometerFunc <- function(Place, PlaceFile=cityfile) # where 'Place' is a city 
   text(x=5, y=1, labels=paste(wdegr, "º (", wdir,")", sep=""), adj=0.5, cex=1.5)
   text(x=0, y=10, labels=Place, adj=0, cex=0.7, col="blue")
   text(x=10, y=10, labels=paste("T: ", temp, " ºC", sep=""), adj=1, cex=0.7, col=temp.col, font=2)
-  text(x=10, y=9.5, labels=paste("H: ", hum, sep=""), adj=1, cex=0.7, col=h.col, font=1)
+  text(x=10, y=9.5, labels=paste("H: ", hum, sep=""), adj=1, cex=0.7, col=h.col, font=2)
+  text(x=10, y=9, labels=paste("P: ", prestxt, sep=""), adj=1, cex=0.7, col=prescolr, font=2)
   text(x=0, y=0, labels=datePlace, adj=0, cex=0.7, col="black")
   text(x=10, y=0, labels=paste(hourPlace, ":", minPlace, sep=""), adj=1, cex=0.7, col="black")
   text(x=0, y=9.5, labels=latDMS, adj=0, cex=0.6, col="black")
